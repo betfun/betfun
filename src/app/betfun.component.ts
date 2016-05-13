47c6ab3a88af5512c701428ed7850edbf27ec019
Davide3 <appiano@gmail.com>
2016-05-13 21:10:23 +0200

    Diffs

--- src/app/betfun.component.ts
+++ src/app/betfun.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './+login';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'betfun-app',
   templateUrl: 'betfun.component.html',
-  styleUrls: ['betfun.component.css']
+  styleUrls: ['betfun.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+@Routes([
+  {path: '/login', component: LoginComponent}
+])
 export class BetfunAppComponent {
   title = 'betfun works!';
 }
